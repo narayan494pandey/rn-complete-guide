5022cc97f3ca4f2ed79f38ed4be3808cb2a0d110
Narayan Pandey <npandey722@gmail.com>
2020-11-30 19:04:54 +0530

    add button

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet,View,Text, TextInput,Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello Aliens</Text>
+    <View>
+      <View>
+        <TextInput />
+        <Button tittle="ADD"/>
+      </View>
+      <View>
+      
+      
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
