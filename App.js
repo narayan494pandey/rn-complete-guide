a464c252cbdfb3bacea4321480c5618b749267f9
Narayan Pandey <npandey722@gmail.com>
2020-12-03 12:36:12 +0530

    delete by press

--- App.js
+++ App.js
@@ -14,6 +14,11 @@ export default function App() {
   const addGoalHandler = (goalTitle) =>{
     setCourseGoals(currentGoals =>[...currentGoals,{id:Math.random().toString(), value:goalTitle}]);
   }
+  const removeGoalHandler = goalId =>{
+    setCourseGoals(currentGoals =>{
+      return currentGoals.filter((goal) => goal.id !== goalId)
+    })
+  }
   
   return (
       <View style={styles.screen}>
@@ -21,7 +26,7 @@ export default function App() {
         <FlatList keyExtractor={(item,index) => item.id}
           data={courseGoals} 
           renderItem={itemData => (
-          <GoalItem onDelete={() => console.log('it works')} title={itemData.item.value} />
+          <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />
         )}>
         
         
