c91cf0e6e22fca18e2174ad29f91b24ed0e71add
Narayan Pandey <npandey722@gmail.com>
2020-12-04 15:10:57 +0530

    cancel button

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -16,6 +16,9 @@ const GoalInput = (props) => {
       props.onAddGoal(enteredGoal);
       setEnteredGoal(' ');
     }
+    const cancelGoalAddHandler = () =>{
+      setIsAddMode(false);
+    }
     return(
         <Modal visible={props.visible} animationType="slide">
           <View style={styles.Container}>
@@ -23,7 +26,8 @@ const GoalInput = (props) => {
               style={styles.input} onChangeText={goalInputHandler}
               value={enteredGoal}
             />
-            <Button title="ADD" onPress={addGoalHandler}/>
+            <Button style={styles.button} title="CANCEL" color="red" onPress={cancelGoalAddHandler} />
+            <Button  style={styles.button} title="ADD" onPress={addGoalHandler}/>
           </View>
         </Modal>
     )
@@ -44,6 +48,11 @@ const styles = StyleSheet.create({
         padding:10,
         marginBottom:10,
       },
+      button:{
+        marginBottom:10,
+        padding:10,
+        borderRadius:30,
+      }
 
 })
 
